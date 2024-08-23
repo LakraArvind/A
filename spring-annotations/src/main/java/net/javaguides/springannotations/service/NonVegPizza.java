646b3905d3d8932745939adeccca09ba427e8212
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-23 18:20:09 +0530

    uising PRIMARY ANNOATATION

--- spring-annotations/src/main/java/net/javaguides/springannotations/service/NonVegPizza.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/service/NonVegPizza.java
@@ -1,9 +1,12 @@
 package net.javaguides.springannotations.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class NonVegPizza implements Pizza{
+
     @Override
     public String getPizza() {
         return "Non-veg Pizza";
