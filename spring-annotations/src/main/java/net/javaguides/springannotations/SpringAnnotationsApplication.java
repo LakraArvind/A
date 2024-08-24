c91cf976a177c666a6f9216923bdfebc2ec2b086
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-24 16:14:41 +0530

    @Repository

--- spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
@@ -2,6 +2,8 @@ package net.javaguides.springannotations;
 
 import net.javaguides.springannotations.controller.MyController;
 import net.javaguides.springannotations.controller.PizzaController;
+import net.javaguides.springannotations.repository.MyRepository;
+import net.javaguides.springannotations.service.MyService;
 import net.javaguides.springannotations.service.VegPizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +18,10 @@ public class SpringAnnotationsApplication {
 //        System.out.println(pizzaController.getPizza() );
         MyController myController = context.getBean(MyController.class);
         System.out.println(myController.hello());
+        MyService myService = context.getBean(MyService.class) ;
+        System.out.println(myService.hello());
+        MyRepository myRepository = context.getBean(MyRepository.class);
+        System.out.println(myRepository.hello());
 //        VegPizza vegPizza = (VegPizza) context.getBean("vegPizza");
 //        System.out.println(vegPizza.getPizza());
     }
