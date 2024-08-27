959727b70b4ec2a5fce8209ef2c3cee78145054b
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-27 10:58:15 +0530

    @properties

--- spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/SpringAnnotationsApplication.java
@@ -3,6 +3,7 @@ package net.javaguides.springannotations;
 import net.javaguides.springannotations.controller.MyController;
 import net.javaguides.springannotations.controller.PizzaController;
 import net.javaguides.springannotations.lazy.LazyLoader;
+import net.javaguides.springannotations.propertysource.PropertySourceDemo;
 import net.javaguides.springannotations.repository.MyRepository;
 import net.javaguides.springannotations.scope.PrototypeBean;
 import net.javaguides.springannotations.scope.SingletonBean;
@@ -18,25 +19,29 @@ public class SpringAnnotationsApplication {
     public static void main(String[] args) {
 
         var context = SpringApplication.run(SpringAnnotationsApplication.class, args);
-        SingletonBean singletonBean1 = context.getBean(SingletonBean.class) ;
-        System.out.println(singletonBean1.hashCode());
-        SingletonBean singletonBean2 = context.getBean(SingletonBean.class) ;
-        System.out.println(singletonBean2.hashCode());
-        SingletonBean singletonBean3 = context.getBean(SingletonBean.class) ;
-        System.out.println(singletonBean3.hashCode());
-        PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class) ;
-        System.out.println(prototypeBean1.hashCode());
-        PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class) ;
-        System.out.println(prototypeBean2.hashCode());
-        PrototypeBean prototypeBean3 = context.getBean(PrototypeBean.class) ;
-        System.out.println(prototypeBean3.hashCode());
-        ValueAnnotationDemo valueAnnotationDemo = context.getBean(ValueAnnotationDemo.class);
-        System.out.println(valueAnnotationDemo.getDefaultName());
-        System.out.println(valueAnnotationDemo.getHost());
-        System.out.println(valueAnnotationDemo.getEmail());
-        System.out.println(valueAnnotationDemo.getPassword());
-        System.out.println(valueAnnotationDemo.getJavaHome());
-        System.out.println(valueAnnotationDemo.getOs());
+//        SingletonBean singletonBean1 = context.getBean(SingletonBean.class) ;
+//        System.out.println(singletonBean1.hashCode());
+//        SingletonBean singletonBean2 = context.getBean(SingletonBean.class) ;
+//        System.out.println(singletonBean2.hashCode());
+//        SingletonBean singletonBean3 = context.getBean(SingletonBean.class) ;
+//        System.out.println(singletonBean3.hashCode());
+//        PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class) ;
+//        System.out.println(prototypeBean1.hashCode());
+//        PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class) ;
+//        System.out.println(prototypeBean2.hashCode());
+//        PrototypeBean prototypeBean3 = context.getBean(PrototypeBean.class) ;
+//        System.out.println(prototypeBean3.hashCode());
+//        ValueAnnotationDemo valueAnnotationDemo = context.getBean(ValueAnnotationDemo.class);
+//        System.out.println(valueAnnotationDemo.getDefaultName());
+//        System.out.println(valueAnnotationDemo.getHost());
+//        System.out.println(valueAnnotationDemo.getEmail());
+//        System.out.println(valueAnnotationDemo.getPassword());
+//        System.out.println(valueAnnotationDemo.getJavaHome());
+//        System.out.println(valueAnnotationDemo.getOs());
+        PropertySourceDemo propertySourceDemo = context.getBean(PropertySourceDemo.class);
+        System.out.println(propertySourceDemo.getHost());
+        System.out.println(propertySourceDemo.getEmail());
+        System.out.println(propertySourceDemo.getPassword());
 
     }
 
