0a17983728d9d7d917add3be3e625eea878b5316
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-28 16:53:24 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -2,10 +2,7 @@ package net.javaguides.springannotations.controller;
 import net.javaguides.springannotations.beans.Book;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Controller
 @RestController
@@ -17,11 +14,7 @@ public class BookController {
     public  String helloWorld(){
         return "Hello World!";
     }
-    @RequestMapping(value= {"/book","/core-java","/java"},
-    method = RequestMethod.GET,
-            produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_XML_VALUE ,MediaType.APPLICATION_JSON_VALUE}
-    )
+    @GetMapping(value= {"/book","/java"})
     //@ResponseBody
     public Book getBook(){
         Book book = new Book( 1,"Core Java" ," learn core java and latest features");
