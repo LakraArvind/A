d2ea69dea6331efba5c01c3bfd301952087990f7
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-28 12:47:58 +0530

    request

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,19 +1,29 @@
 package net.javaguides.springannotations.controller;
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController
+@RequestMapping("/api")
 public class BookController {
 
+
     @RequestMapping("/hello-world")
-    @ResponseBody
+    //@ResponseBody
     public  String helloWorld(){
         return "Hello World!";
     }
-    @RequestMapping("/book")
-    @ResponseBody
+    @RequestMapping(value = {"/book", "/core-java", "/java"},
+    method = RequestMethod.GET,
+    produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+   // @ResponseBody
     public Book getBook(){
         Book book = new Book( 1,"Core Java" ," learn core java and latest features");
         return book;
