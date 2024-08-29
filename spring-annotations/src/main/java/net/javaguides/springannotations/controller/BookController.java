bc65f6c0fc78f03372b32e2d3148cb25df49fdd2
Arvind Lakra <arvindlakra50398@gmail.com>
2024-08-29 11:14:19 +0530

    RESPONSE

--- spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
+++ spring-annotations/src/main/java/net/javaguides/springannotations/controller/BookController.java
@@ -1,6 +1,9 @@
 package net.javaguides.springannotations.controller;
+
 import net.javaguides.springannotations.beans.Book;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +14,26 @@ public class BookController {
 
     @RequestMapping("/hello-world")
     //@ResponseBody
-    public  String helloWorld(){
+    public String helloWorld() {
         return "Hello World!";
     }
-    @GetMapping(value= {"/book","/java"})
+
+    @GetMapping(value = {"/book", "/java"})
     //@ResponseBody
-    public Book getBook(){
-        Book book = new Book( 1,"Core Java" ," learn core java and latest features");
+    public Book getBook() {
+        Book book = new Book(1, "Core Java", " learn core java and latest features");
         return book;
+    }
+
+    @PostMapping(value = "/books/create",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    //@ResponseStatus (value = HttpStatus.CREATED)
+
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+        return new ResponseEntity<>(book,HttpStatus.CREATED);
     }
 }
